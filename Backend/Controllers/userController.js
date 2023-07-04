ba6a25b6cab99f064a9b45304a8dc5773e486382
mohamedabdiahmed <mohamedahmedabdiah@hotmail.com>
2023-07-04 18:24:09 +0300

    Done

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -1,5 +1,6 @@
 const {PrismaClient} = require('@prisma/client')
 const prisma = new PrismaClient();
+const bcryptjs = require('bcryptjs')
 
 
 // Get all Users
@@ -27,11 +28,18 @@ const getUsers = async(req,res)=>{
 // new User 
 const userPost = async(req,res)=>{
     const {userName, email, password} =req.body;
+    if(!userName || !email || !password){
+        res.status(404).json({
+            message: "waan ka xunnahay macmiil ma jiro userkan!"
+        })
+    }
+    const salt = await bcryptjs.genSaltSync(10);
+    const hashedPassword = await bcryptjs.hashSync(password, salt)
     const newUser = await prisma.User.create({
         data:{
             userName,
             email,
-            password
+            password:hashedPassword
         }
     });
     try {
@@ -67,7 +75,6 @@ const updateUser = async(req,res)=>{
 
 // Deleted
 
-// Update User
 const deleteUser = async(req,res)=>{
     const {idUser} = req.params
     const {userName} = req.body;
@@ -85,10 +92,36 @@ const deleteUser = async(req,res)=>{
         })
     }
 }
+
+// GetByid
+const findUser = async(req,res)=>{
+    const {byId} = req.params
+    const {userName,email,password} = req.body;
+
+    try {
+        const findUsers = await prisma.User.findUnique({
+            where:{
+                userId:+byId,
+                userName,
+                email,
+                password
+            },
+        })
+        res.json({
+            message: `find User ${byId}`,
+            findUsers
+            // status: "waa lasoo heley xogta"
+          });
+    } catch (error) {
+        res.status(404).json({
+            message: `Error Finding user`
+        })
+    }
+}
 module.exports = {
     getUsers,
     userPost,
     updateUser,
-    deleteUser
-
+    deleteUser,
+    findUser
 }
--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -3,6 +3,7 @@ const routes = express.Router()
 const userController = require('../Controllers/userController')
 
 routes.get('/all',userController.getUsers)
+routes.get('/getById/:byId',userController.findUser)
 routes.post('/new',userController.userPost)
 routes.put('/update/:idUser',userController.updateUser)
 routes.delete('/delete/:idUser',userController.deleteUser)
